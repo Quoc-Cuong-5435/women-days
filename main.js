df0609b3e6d8476e7caa8989cb16cdea26764026
Huỳnh Quốc Cường <56816884+quoccuong5435@users.noreply.github.com>
2024-10-18 18:54:03 +0700

    Update main.js
    Signed-off-by: Huỳnh Quốc Cường <56816884+quoccuong5435@users.noreply.github.com>

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", function() {
     const lastListItem = document.querySelector(".card-info ul li:last-child");
     const readMoreButton = document.querySelector(".read-more");
 
-    lastListItem.addEventListener("mouseenter", function() {
-        readMoreButton.style.display = "block";
-    });
+    // lastListItem.addEventListener("mouseenter", function() {
+    //     readMoreButton.style.display = "block";
+    // });
 
     // lastListItem.addEventListener("mouseleave", function() {
     //     readMoreButton.style.display = "none";
